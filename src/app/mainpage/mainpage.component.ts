144830fd6d0510c9e65cd965f16f1519f5bacf2b
hemant <hemant1096@outlook.com>
2024-09-11 15:28:14 +0530

    commit

--- src/app/mainpage/mainpage.component.ts
+++ src/app/mainpage/mainpage.component.ts
@@ -1,13 +1,41 @@
-import { Component } from '@angular/core';
-import {MatSidenavModule} from '@angular/material/sidenav';
+import { Component, OnInit } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable,of } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { MatToolbarModule } from '@angular/material/toolbar'; // Import MatToolbarModule
+import { MatListModule } from '@angular/material/list'; // Import MatListModule
+import { MatIconModule } from '@angular/material/icon'; // Import MatIconModule
+import { CommonModule } from '@angular/common';
+import {MatTabsModule} from '@angular/material/tabs';
 
 @Component({
   selector: 'app-mainpage',
   standalone: true,
-  imports: [MatSidenavModule],
+  imports: [
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule,
+    MatIconModule,
+    CommonModule,
+    MatTabsModule
+  ],
   templateUrl: './mainpage.component.html',
   styleUrl: './mainpage.component.css'
 })
-export class MainpageComponent {
+export class MainpageComponent implements OnInit {
+  isHandset$: Observable<boolean> = of(false);
 
+  // Constructor initializes BreakpointObserver
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+  // Initialization logic in ngOnInit
+  ngOnInit(): void {
+    this.isHandset$ = this.breakpointObserver.observe(Breakpoints.Handset)
+      .pipe(
+        map(result => result.matches)
+      );
+  }
 }
+
+
